d7cb7eef4d52c12cdc5672339b1922c263d2d122
Snah-s <camiloernestosoto@gmail.com>
2024-07-18 12:24:22 -0500

    🐛 Set handler for non existant files

--- serverless/lambda_download.py
+++ serverless/lambda_download.py
@@ -9,13 +9,18 @@ def lambda_handler(event, context):
     file_name = event['file_name']
 
     try:
+        s3.head_object(
+            Bucket=bucket_name,
+            Key=f"{user_id}/{file_name}"
+        )
+        
         pre_signed_url = s3.generate_presigned_url(
             'get_object',
             Params={
                 'Bucket': bucket_name,
                 'Key': f"{user_id}/{file_name}"
             },
-            ExpiresIn=1800 # Se expira en 1 h
+            ExpiresIn=1800 # La URL expirará en 30 minutos
         )
 
         return {
@@ -24,6 +29,11 @@ def lambda_handler(event, context):
                 'file_name': file_name,
                 'download_url': pre_signed_url
             })
+        }
+    except s3.exceptions.NoSuchKey:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'error': 'File not found'})
         }
     except Exception as e:
         return {
