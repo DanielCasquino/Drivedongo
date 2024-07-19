d4c657f929c0f4f53baf3ce45ab36bc2390f15aa
DanielCasquino <daniel.casquino@utec.edu.pe>
2024-07-19 05:29:00 -0500

    aaaaaaaaaaaaaaaaa

--- serverless/lambda_get.py
+++ serverless/lambda_get.py
@@ -1,19 +1,9 @@
 import boto3
 import json
 import base64
-from PIL import Image
-from io import BytesIO
 
 s3 = boto3.client('s3')
 bucket_name = 'bucket-drive-main'
-
-def resize_image(image_data, scale_factor):
-    image = Image.open(BytesIO(image_data))
-    new_size = (image.width // scale_factor, image.height // scale_factor)
-    resized_image = image.resize(new_size, Image.ANTIALIAS)
-    buffer = BytesIO()
-    resized_image.save(buffer, format=image.format)
-    return buffer.getvalue()
 
 def lambda_handler(event, context):
     user_id = event['user_id']
@@ -27,22 +17,21 @@ def lambda_handler(event, context):
         files = []
         if 'Contents' in response:
             for obj in response['Contents']:
-                file_key = obj['Key']
-                file_name = file_key[len(f"{user_id}/"):]
+                if obj['Key'] == f"{user_id}/":
+                    continue
                 file_info = {
-                    'file_name': file_name,
+                    'file_name': obj['Key'],
                     'upload_date': obj['LastModified'].strftime('%Y-%m-%d %H:%M:%S'),
-                    'file_type': file_key.split('.')[-1],
+                    'file_type': obj['Key'].split('.')[-1],
                     'size': obj['Size']
                 }
 
                 if file_info['file_type'].lower() in ['jpg', 'jpeg', 'png', 'gif']:
                     image_response = s3.get_object(
                         Bucket=bucket_name,
-                        Key=file_key
+                        Key=obj['Key']
                     )
-                    resized_image_data = resize_image(image_response['Body'].read(), 4)
-                    file_info['image'] = base64.b64encode(resized_image_data).decode('utf-8')
+                    file_info['image'] = base64.b64encode(image_response['Body'].read()).decode('utf-8')
                 else:
                     file_info['image'] = None
 
@@ -51,10 +40,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'body': 'Files retrieved successfully',
-            'payload': {
-                'user_id': user_id,
-                'files': files
-            }
+            'payload': files
         }
     except Exception as e:
         return {
